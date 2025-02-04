e3b9df2f1264f0c05f55b165dc36904b38acd78e
Eddie <nm_eddie@TAICHISGN1.LOCAL>
2025-02-04 17:27:16 +0800

    setup auth

---
+++ client/src/utils/api.ts
@@ -0,0 +1,10 @@
+export const API_BASE_URL = "http://localhost:3002"
+
+export const API_ROUTES = {
+    AUTH: `${API_BASE_URL}/api/auth`,
+    PRODUCTS: `${API_BASE_URL}/api/products`,
+    ORDERS: `${API_BASE_URL}/api/orders`,
+    CART: `${API_BASE_URL}/api/cart`,
+    USERS: `${API_BASE_URL}/api/users`,
+    PAYMENT: `${API_BASE_URL}/api/payment`,
+}
